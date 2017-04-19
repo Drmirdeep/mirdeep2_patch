3727eae6f785e9b1bca658b1618dc69b7994a2d3
Drmirdeep <Drmirdeep@users.noreply.github.com>
2017-04-19 15:38:33 +0200

    Update

--- patchme.sh
+++ patchme.sh
@@ -5,3 +5,10 @@ CWD=$(pwd -LP)
 echo export PATH=$CWD:\$PATH >> ~/.bash_profile
 
 chmod u+x *.pl
+
+if [[ -f "$CWD/miRDeep2_patch.pl" ]];then 
+	a=$(which miRDeep2.pl)
+	mv $a $a.orig
+	ln -s $CWD/miRDeep2_patch.pl $a
+	echo File $a has been moved to $a.orig and the current $CWD/miRDeep2_patch.pl has been linked to $a
+fi
